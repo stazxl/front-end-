f45b43826c8fa285fc29998c593b4fcd97f925d7
Keryan <keryan@assur-abo.fr>
2023-05-31 11:16:18 +0200

    Fix the appta make it running correctly

--- src/App.js
+++ src/App.js
@@ -10,28 +10,21 @@ import {
 import jsonServerProvider from 'ra-data-json-server';
 
 //Header
-const httpClient = (url, options = {}) => {
-  if (!options.headers) {
-    options.headers = new Headers({ Accept: 'application/json' });
-  }
-  const token = JSON.parse(localStorage.getItem('token'));
-  options.headers.set('Access-Control-Expose-Headers','Content-Range');
+// const httpClient = (url, options = {}) => {
+//   if (!options.headers) {
+//     options.headers = new Headers({ Accept: 'application/json' });
+//   }
+//   options.headers.set('Access-Control-Expose-Headers','Content-Range');
   
-  return fetchUtils.fetchJson(url, options);
-};
+//   return fetchUtils.fetchJson(url, options);
+// };
 
 export const backURL ="http://localhost:5000"
 
-const dataProvider = jsonServerProvider(apiURL, httpClient);
+//const dataProvider = jsonServerProvider(backURL, httpClient);
 const dataProviderGuesser = jsonServerProvider('https://jsonplaceholder.typicode.com')
 
 function App(){
-  const token = localStorage.getItem('token')
-
-  if(!token) {
-    return <Login />
-  }
-
   return (
     <Admin dataProvider={dataProviderGuesser}>        
       <Resource name="Items" list={ListGuesser}/>
