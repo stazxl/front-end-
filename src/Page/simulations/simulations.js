045d9b297133c16a98207226d0f5830c484491f0
stazxl <florian.santos.40@gmail.com>
2023-06-26 14:59:50 +0200

    update

--- src/Page/simulations/simulations.js
+++ src/Page/simulations/simulations.js
@@ -1,107 +1,97 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { List, Datagrid, DateField, TextField } from 'react-admin';
-import {useParams } from 'react-router-dom';
-import {Chart} from 'chart.js';
-import {  Box, } from '@mui/material';
+import { useParams } from 'react-router-dom';
+import { Chart } from 'chart.js';
+import { Box } from '@mui/material';
 import axios from 'axios';
-import {useQuery} from '@tanstack/react-query'
-
-
+import { useQuery } from '@tanstack/react-query';
 
 export const SimulationList = () => {
-  return(
+  return (
     <Box>
       <List>
         <Datagrid bulkActionButtons={false} rowClick="edit">
-          <TextField source="nomEntreprise"/>
-          <TextField source="nombreAbonnes"/>
-          <TextField source="nouveauxInscrisMois"/>
-          <TextField source="price"/>
-          <TextField source="CasMédicaux"/>
-          <TextField source="déménagement"/>
-          <TextField source="lignesImpayeesMois"/>
-          <TextField source="suspensionPro"/>
-        
+          <TextField source="nomEntreprise" />
+          <TextField source="nombreAbonnes" />
+          <TextField source="nouveauxInscrisMois" />
         </Datagrid>
       </List>
     </Box>
-  )
+  );
 };
+
 export const SimulationEdit = () => {
   const [nombreMaladies, setNombreMaladies] = useState(0);
   const [prixAbonnement, setPrixAbonnement] = useState(0);
   const [montantTotal, setMontantTotal] = useState(0);
-  useEffect(() => {
-    // Créer et mettre à jour le graphique lorsque les données changent
-    const speedData = {
-      labels: ['0', '10', '20', '30', '40', '50', '60'],
-      datasets: [
-        {
-          label: 'test',
-          data: [0, 59, 75, 20, 20, 55, 40],
-          tension: 0.4,
-          cubicInterpolationMode: 'monotone',
-          fill: false,
-          borderColor: '#E64A19',
-          backgroundColor: 'transparent',
-          borderDash: [20, 10, 60, 10],
-          pointBorderColor: '#E64A19',
-          pointBackgroundColor: '#FFA726',
-          pointRadius: 5,
-          pointHoverRadius: 10,
-          pointHitRadius: 30,
-          pointBorderWidth: 4,
-          pointStyle: 'rectRounded',
-        },
-      ],
-    };
-
-    const speedCanvas = speedCanvasRef.current;
-    const lineChart = new Chart(speedCanvas, {
-      type: 'line',
-      data: speedData,
-    });
-
-    // Nettoyer le graphique lorsqu'il est démonté
-    return () => {
-      lineChart.destroy();
-    };
-  }, [nombreMaladies, prixAbonnement]);
-  
   const speedCanvasRef = useRef(null);
-  const params = useParams()
-  console.log('params',params)
-  
- // Getting providers info
-      const urlData = {
-        url:'Simulations/'+params.id,
-        baseURL:'http://localhost:5000/',
-        method:'get',
-      }; 
-    let requestData = axios(urlData)
+  const params = useParams();
+  console.log('params', params);
 
-  const { isLoading, isError, data, error } = useQuery({
-    queryKey: ['SimulationProviders n° '+params.id],
-    queryFn: requestData,
-  })
+  useEffect(() => {
+    if (speedCanvasRef.current) {
+      const speedData = {
+        labels: ['0', '10', '20', '30', '40', '50', '60'],
+        datasets: [
+          {
+            label: 'test',
+            data: [0, 59, 75, 20, 20, 55, 40],
+            tension: 0.4,
+            cubicInterpolationMode: 'monotone',
+            fill: false,
+            borderColor: '#E64A19',
+            backgroundColor: 'transparent',
+            borderDash: [20, 10, 60, 10],
+            pointBorderColor: '#E64A19',
+            pointBackgroundColor: '#FFA726',
+            pointRadius: 5,
+            pointHoverRadius: 10,
+            pointHitRadius: 30,
+            pointBorderWidth: 4,
+            pointStyle: 'rectRounded',
+          },
+        ],
+      };
+
+      const lineChart = new Chart(speedCanvasRef.current, {
+        type: 'line',
+        data: speedData,
+      });
+
+      return () => {
+        lineChart.destroy();
+      };
+    }
+  }, [nombreMaladies, prixAbonnement]);
+
+// Getting providers info
+const urlData = {
+  url: 'Simulations/' + params.id,
+  baseURL: 'http://localhost:5000/',
+  method: 'get',
+};
+const queryFn = async () => {
+  const response = await axios(urlData);
+  return response.data;
+};
+
+const { isLoading, isError, data, error } = useQuery({
+  queryKey: ['SimulationProviders n° ' + params.id],
+  queryFn: queryFn,
+});
 
   if (isLoading) {
-    return <span>Loading...</span>
-  }
-
-  else if (isError) {
-    return <span>Error: {error.message}</span>
-  }
-  else{
-    console.log('data',data)
- 
+    return <span>Loading...</span>;
+  } else if (isError) {
+    return <span>Error: {error.message}</span>;
+  } else {
+    console.log('data', data);
 
     const calculerMontantTotal = () => {
       const resultatCalcul = nombreMaladies * prixAbonnement;
       setMontantTotal(resultatCalcul);
     };
-  
-  
+
     return (
       <div>
         <h1>Calcul du montant total</h1>
@@ -114,7 +104,7 @@ export const SimulationEdit = () => {
             onChange={(e) => setNombreMaladies(Number(e.target.value))}
           />
         </div>
-  
+
         <div>
           <label htmlFor="prixAbonnement">Prix de l'abonnement :</label>
           <input
@@ -126,10 +116,9 @@ export const SimulationEdit = () => {
         </div>
         <button onClick={calculerMontantTotal}>Calculer</button>
         <p>Montant total : {montantTotal}</p>
-        <Box sx={{ width:'800px' }}>
+        <Box sx={{ width: '800px' }}>
           <canvas ref={speedCanvasRef} />
         </Box>
-        
       </div>
     );
   }
